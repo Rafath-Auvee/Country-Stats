9b2a951ad58bdbdb5724d7b47ef6e6d9242935b8
Rafath-Auvee <rbza98@outlook.com>
2023-04-15 13:46:00 +0600

    tables added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 import Layout from "./components/Layout/Layout";
+import SearchInput from "./components/SearchInput/SearchInput";
+import Countries from "./components/CountriesTable/CountriesTable";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return (
     <>
-      <Layout>Main</Layout>
+      <Layout>
+        <div className={styles.inputContainer}>
+          <div className={styles.counts}>
+            Found {countries.length} countries
+          </div>
+          <div className={styles.input}>
+            <SearchInput
+              placeholder="Filter by Name, Region or SubRegion"
+              // onChange={onInputChange}
+            />
+          </div>
+        </div>
+        <Countries countries={countries}/>
+      </Layout>
     </>
   );
 }
